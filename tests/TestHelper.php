77f47936775496ae2072c84a956c9061c0ab6e59
Guillaume Luchet <guillaume.luchet@gmail.com>
2010-03-30 10:01:14 +0200

    Add package tag

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *
- *
+ * @package Litmus_UnitTest
  * @author Guillaume <guillaume@geelweb.org>
  * @copyright Copyright (c) 2010, Guillaume Luchet
  * @license http://opensource.org/licenses/bsd-license.php BSD License
