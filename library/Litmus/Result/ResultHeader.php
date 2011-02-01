ed956862afa439a1627bd77dda27de1d575a39a8
Guillaume Luchet <guillaume@geelweb.org>
2011-02-01 18:11:35 +0100

    Manage spam result details

---
+++ library/Litmus/Result/ResultHeader.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *
+ * @package Litmus
+ * @author Guillaume <guillaume@geelweb.org>
+ * @copyright Copyright (c) 2010, Guillaume Luchet
+ * @license http://opensource.org/licenses/bsd-license.php BSD License
+ */
+
+require_once('Litmus/Result/ResultHeader/Spam.php');
+
+/**
+ *
+ * @package Litmus
+ */
+class Litmus_Result_ResultHeader
+{
+    public static function load($xml)
+    {
+        if ($xml instanceof DOMElement) {
+            $dom = $xml;
+        } else {
+            $dom = new DOMDocument();
+            $dom->loadXML($xml);
+        }
+        $lst = $dom->getElementsByTagName('result_header');
+        $col = array();
+        foreach($lst as $item) {
+            $obj = new Litmus_Result_ResultHeader();
+            foreach ($item->childNodes as $child) {
+                $property = $child->nodeName;
+                $obj->$property = $child;
+            }
+            array_push($col, $obj);
+        }
+        return $col;
+
+    }
+
+    public function __set($property, $value)
+    {
+        switch ($property) {
+            case 'spam_header':
+                $this->$property = Litmus_Result_ResultHeader_Spam::load($value);
+                break;
+        }
+    }
+}
---
+++ library/Litmus/Result/ResultHeader/Spam.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * @package Litmus
+ * @author Guillaume <guillaume@geelweb.org>
+ * @copyright Copyright (c) 2010, Guillaume Luchet
+ * @license http://opensource.org/licenses/bsd-license.php BSD License
+ */
+
+/**
+ *
+ * @package Litmus
+ */
+class Litmus_Result_ResultHeader_Spam
+{
+    public static function load($xml)
+    {
+        if ($xml instanceof DOMElement) {
+            $dom = $xml;
+        } else {
+            $dom = new DOMDocument();
+            $dom->loadXML($xml);
+        }
+        $obj = new Litmus_Result_Testing_Application();
+        foreach ($dom->childNodes as $child) {
+            $property = $child->nodeName;
+            $obj->$property = $child;
+        }
+        return $obj;
+    }
+
+    public function __set($property, $value)
+    {
+        switch ($property) {
+            case 'description':
+            case 'rating':
+            case 'key':
+                $this->$property = $value->nodeValue;
+                break;
+        }
+    }
+}
